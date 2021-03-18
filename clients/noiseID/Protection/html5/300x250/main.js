47d7fb63c635111741c978b515e74257abdb9ef9
gutenberg-digital <47384481+gutenberg-digital@users.noreply.github.com>
2021-03-18 11:22:09 +0000

    Add files via upload

--- clients/noiseID/Protection/html5/300x250/main.js
+++ clients/noiseID/Protection/html5/300x250/main.js
@@ -38,7 +38,7 @@ function animate(){
       .to(iamsLogoPaths, 0, { fill: '#F2F2F2' })
       .to('#bg', 1, { autoAlpha: 1 })
       .to('#logo', 0.5, { autoAlpha: 1 }, "-=0.5")
-      .to(['#bg'], 0.75, { scale: 1.35, ease: Power1.easeInOut }, '-=0.75')
+      .to(['#bg'], 0.75, { scale: 1.35, ease: Power1.easeInOut }, '+=0.5')
       //  Target looking for dog nose
       .to('#target', 0.67, { scale: 1, ease: Power4.easeOut }, "-=0.5")
       .to('#text_container', 0.5, { x: 0, ease: Power1.easeOut }, "+=0")
